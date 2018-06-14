2e21a9a50a17e7f656a6bae0bfe2f4cc710f3bae
Chorwonui <upheaval212@gmail.com>
2018-06-14 17:18:03 +0900

    ???

--- BOJ/DFS&BFS/1014.py
+++ BOJ/DFS&BFS/1014.py
@@ -1,25 +1,38 @@
 import sys
 Read = sys.stdin.readline
 
+
+def dfs(x):
+    if vis[x] == count:
+        return 0
+    vis[x] = count
+    for item in adj[x]:
+        if match[item] == -1 or dfs(match[item]):
+            match[item] = x
+            return 1
+    return
+
+
 for _ in range(int(Read())):
     N, M = map(int, Read().split())
     classroom = [Read() for _ in range(N)]
     num = [[0 for _ in range(M)] for _ in range(N)]
-    acnt = 0
-    bcnt = 0
+    even_cnt = 0
+    odd_cnt = 0
 
     for i in range(N):
         for j in range(M):
             if classroom[i][j] == 'x':
                 continue
             if j % 2:
-                num[i][j] = acnt
-                acnt += 1
+                num[i][j] = even_cnt
+                even_cnt += 1
             else:
-                num[i][j] = bcnt
-                bcnt += 1
+                num[i][j] = odd_cnt
+                odd_cnt += 1
 
-    adj = [[] for _ in range(acnt)]
+    adj = [[] for _ in range(even_cnt)]
+
     for i in range(N):
         for j in range(M):
             if classroom[i][j] == 'x':
@@ -32,23 +45,12 @@ for _ in range(int(Read())):
                         if 0 <= nx < N and 0 <= ny < M and classroom[nx][ny] == '.':
                             adj[num[i][j]].append(num[nx][ny])
 
-    vis = [0] * acnt
-    match = [-1] * bcnt
-    vcnt = 0
-
-    def dfs(cur):
-        if vis[cur] == vcnt:
-            return 0
-        vis[cur] = vcnt
-        for nxt in adj[cur]:
-            if match[nxt] == -1 or dfs(match[nxt]):
-                match[nxt] = cur
-                return 1
-        return 0
-
+    vis = [0] * even_cnt
+    match = [-1] * odd_cnt
+    count = 0
 
     matched = 0
-    for i in range(acnt):
-        vcnt += 1
+    for i in range(even_cnt):
+        count += 1
         matched += dfs(i)
-    print(acnt + bcnt - matched)
+    print(even_cnt + odd_cnt - matched)
