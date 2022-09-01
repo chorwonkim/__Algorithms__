d59f47649b52b38ad59bd051178ec9bd9eb6d9ae
chorwonkim <upheaval212@gmail.com>
2022-09-01 14:34:02 +0900

    git push testing2

--- BOJ/Implementation/10820.py
+++ BOJ/Implementation/10820.py
@@ -1,19 +1,24 @@
+from sys import stdin
+Read = stdin.readline
+
 while True:
-    try:
-        result = [0 for _ in range(4)]
-        data = input()
+    result = [0 for _ in range(4)]
+    data = Read().rstrip('\n')
 
-        for i in data:
-            if 'A' <= i <= 'Z':
-                result[1] += 1
-            elif 'a' <= i <= 'z':
-                result[0] += 1
-            elif '0' <= i <= '9':
-                result[2] += 1
-            elif i == ' ':
-                result[3] += 1
-
-        for i in result:
-            print(i, end=' ')
-    except EOFError:
+    if not data:
         break
+
+    for i in data:
+        if 'A' <= i <= 'Z':
+            result[1] += 1
+        elif 'a' <= i <= 'z':
+            result[0] += 1
+        elif '0' <= i <= '9':
+            result[2] += 1
+        elif i == ' ':
+            result[3] += 1
+
+    for i in result:
+        print(i, end=' ')
+    print()
+
